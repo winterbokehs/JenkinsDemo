04d0798cf737334237b396965c8c059a173f2a29
yangyishan <yangyishan@pinming.cn>
2021-12-17 13:24:17 +0800

    change test2

--- src/main/java/com/example/demo/controller/JSONCreateController.java
+++ src/main/java/com/example/demo/controller/JSONCreateController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+//@RequestMapping("/api")
 @Api(value = "获取JSON的Controller")
 public class JSONCreateController {
     @Autowired
